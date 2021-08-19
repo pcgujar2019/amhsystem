0dbafbca5f1bc288fe5b833dd1b1ca48b3c6f102
pcgujar2019 <gujarprashant89@gmail.com>
2021-08-19 10:49:39 +0530

    Chnaged DB settinds

--- amhsystem/settings.py
+++ amhsystem/settings.py
@@ -90,16 +90,24 @@ WSGI_APPLICATION = 'amhsystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'amhdb',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
+        'USER': 'pcgujar',
+        'PASSWORD': 'Pdjrksogdj',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '',
+        }
     }
-}
 
 
 
@@ -143,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
